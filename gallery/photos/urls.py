7f33a846c89c8a50f85302b564ca4d018de05508
vincentmuya <vincentmuya13@gmail.com>
2018-02-24 12:40:48 +0300

    imported project setting to photos/url

--- gallery/photos/urls.py
+++ gallery/photos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
@@ -6,3 +8,6 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
