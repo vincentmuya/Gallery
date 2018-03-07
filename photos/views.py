709ce090e6607f6504ea0367b1c52a4e32e30690
vincentmuya <vincentmuya13@gmail.com>
2018-03-08 02:48:54 +0300

    removed date and time setting

--- photos/views.py
+++ photos/views.py
@@ -1,31 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-import datetime as dt
 from django.http  import HttpResponse,Http404
 from .models import Image
 
 # Create your views here.
 def photos_today(request):
-    date = dt.date.today()
     photos = Image.todays_photos()
-    return render(request, 'all-photos/today-photos.html', {"date": date,"photos":photos})
-
-def past_days_photos(request,past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(photos_today)
-
-    photos = Article.days_photos(date)
-    return render(request, 'all-photos/past-photos.html', {"date": date})
-
+    return render(request, 'all-photos/today-photos.html', {"photos":photos})
 
 def search_results(request):
 
